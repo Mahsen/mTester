91f325b295a886c63a9626ae2bccf7bcd9a69e07
Mahsen <not.select@gmail.com>
2025-02-08 04:24:08 +0000

    Add SQLite library

--- Application/Web/Default.cpp
+++ Application/Web/Default.cpp
@@ -31,6 +31,7 @@
 #include <map>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <sqlite3.h>
 #include "../Module/Print.hpp"
 #include "../nlohmann/json.hpp"
 /************************************************** Defineds
@@ -138,8 +139,48 @@ cJSON* getNetworkInterfaces() {
 
     return json_Respose;
 }*/
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Callback function to process query results
+static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
+    for (int i = 0; i < argc; i++) {
+        Print((char*)azColName[i]);
+        Print((char*)" = ");
+        Println((char*)(argv[i] ? argv[i] : "NULL"));
+    }
+    Println((char*)"----------------------");
+    return 0;
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
 /* The main function start of program in cpp language */
 int main() {
+Println((char *)"Application: Start");
+
+    sqlite3* db;
+    char* errorMessage = 0;
+
+    // Open database (creates if not exists)
+    if (sqlite3_open("database.db", &db)) {
+        Print((char*)"Error opening database: ");
+        Println((char*)sqlite3_errmsg(db));
+        return 1;
+    }
+
+    // SQL commands
+    const char* sqlClearData = "DELETE FROM Users;";
+    const char* sqlCreateTable = "CREATE TABLE IF NOT EXISTS Users (ID INTEGER PRIMARY KEY, Name TEXT, Age INTEGER);";
+    const char* sqlInsertData = "INSERT INTO Users (Name, Age) VALUES ('Mohammad', 21), ('Zohre', 20);";
+    const char* sqlSelectData = "SELECT * FROM Users;";
+
+    // Execute SQL statements
+    sqlite3_exec(db, sqlClearData, 0, 0, &errorMessage);
+    sqlite3_exec(db, sqlCreateTable, 0, 0, &errorMessage);
+    sqlite3_exec(db, sqlInsertData, 0, 0, &errorMessage);
+    sqlite3_exec(db, sqlSelectData, callback, 0, &errorMessage);
+
+    // Close database
+    sqlite3_close(db);
+    return 0;
+
 /*
     // Get the content length
     //char buffer[1024];
@@ -179,8 +220,8 @@ int main() {
 
     */
 
-    Print((char*)"{\"error\": \"No data received\"}\n");
-    return 0;
+    //Print((char*)"{\"error\": \"No data received\"}\n");
+    //return 0;
 }
 /************************************************** Tasks
  * *************************************************************/
